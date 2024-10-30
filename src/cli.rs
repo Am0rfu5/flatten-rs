d2494fc94e49b151fe583a4f3230bb7d8d7233c2
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2024-10-30 12:56:49 -0400

    added rustdoc comments to cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -1,25 +1,37 @@
 use structopt::StructOpt;
 use std::path::PathBuf;
 
+/// Command-line interface options for the `flattenrs` tool.
 #[derive(StructOpt)]
 pub struct Cli {
     /// The directory to flatten
+    ///
+    /// Defaults to the current directory if not specified. 
     #[structopt(parse(from_os_str), default_value = ".")]
     pub directory: PathBuf,
-
-    /// The output file
+    
+    /// The output file where the flattened content will be saved.
+    ///
+    /// If not specified, an output file with a timestamped filename will be created.
     #[structopt(parse(from_os_str), short, long)]
     pub output: Option<PathBuf>,
 
-    /// Files to exclude
+    /// Files or directories to exclude during flattening.
+    ///
+    /// These paths will be ignored in the final output.
     #[structopt(parse(from_os_str), short, long)]
     pub exclude: Vec<PathBuf>,
 
-    /// Files to include
+    /// Files or directories to include explicitly during flattening.
+    ///
+    /// Only the files specified in this list will be processed.
     #[structopt(parse(from_os_str), short, long)]
     pub include: Vec<PathBuf>,
 
-    /// Allow hidden files
+    /// Allow hidden files to be included in the output.
+    ///
+    /// If this flag is set, hidden files (those starting with a dot) will also
+    /// be processed.
     #[structopt(short, long)]
     pub allow_hidden: bool,
 }
